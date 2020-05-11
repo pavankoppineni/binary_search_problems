fb9b7a2a572810169059e6ceb95f19da647312a8
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-05-11 23:22:54 +0530

    Added binary search problems

---
+++ src/BinarySearchProblems.Shell/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchProblems.Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/BinarySearchProblems/FindNumberInSortedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchProblems
+{
+    /// <summary>
+    /// Problem : Find a number in sorted array
+    /// </summary>
+    public class FindNumberInSortedArray
+    {
+        public int Find(int[] values, int number)
+        {
+            var low = 0;
+            var high = values.Length - 1;
+            return Find(values, number, low, high);
+        }
+
+        private int Find(int[] values, int number, int low, int high)
+        {
+            if (low > high)
+            {
+                return -1;
+            }
+
+            var mid = (low + high) / 2;
+            if (values[mid] == number)
+            {
+                return mid;
+            }
+            else if (number > values[mid])
+            {
+                return Find(values, number, mid + 1, high);
+            }
+            else
+            {
+                return Find(values, number, low, mid - 1);
+            }
+        }
+    }
+}
