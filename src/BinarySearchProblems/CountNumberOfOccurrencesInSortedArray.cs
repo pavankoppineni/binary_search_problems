206ae85df1c054362c5de501c6fffbe055247339
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-12 00:26:05 +0530

    Added new binary search problem (#2)
    * Added binary search problems
    
    * Added ReadMe file
    
    * Add ReadMe file
    
    * Added new problem : Count number of occurances of a number in a sorted array.
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/BinarySearchProblems/CountNumberOfOccurrencesInSortedArray.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchProblems
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/count-number-of-occurrences-or-frequency-in-a-sorted-array/
+    /// </summary>
+    public class CountNumberOfOccurrencesInSortedArray
+    {
+        public int Count(int[] values, int number)
+        {
+            var count = 0;
+            var low = 0;
+            var high = values.Length - 1;
+            var index = Find(values, number, low, high);
+            if (index < 0)
+            {
+                return -1;
+            }
+
+            var rightIndex = index;
+            while (rightIndex < values.Length)
+            {
+                if (values[rightIndex] == number)
+                {
+                    count++;
+                    rightIndex++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var leftIndex = index - 1;
+            while (leftIndex >= 0)
+            {
+                if(values[leftIndex] == number)
+                {
+                    count++;
+                    leftIndex--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+
+        private int Find(int[] values, int number, int low, int high)
+        {
+            if (low > high)
+            {
+                return -1;
+            }
+
+            var mid = (high + low) / 2;
+            if (values[mid] == number)
+            {
+                return mid;
+            }
+            else if (values[mid] > number)
+            {
+                return Find(values, number, low, mid - 1);
+            }
+            else
+            {
+                return Find(values, number, mid + 1, high);
+            }
+        }
+    }
+}
---
+++ test/BinarySearchProblems.Tests/CountNumberOfOccurrencesInSortedArrayTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchProblems.Tests
+{
+    [TestClass]
+    public class CountNumberOfOccurrencesInSortedArrayTests
+    {
+        private static IEnumerable<object[]> GetDataSet()
+        {
+            var dataSet = new List<object[]>();
+
+            var case_one = new object[]
+            {
+                new int[] { 1, 1, 2, 2, 2, 2, 3 },
+                2,
+                4
+            };
+            var case_two = new object[]
+            {
+                new int[] { 1, 1, 2, 2, 2, 2, 3 },
+                3,
+                1
+            };
+            var case_three = new object[]
+            {
+                new int[] { 1, 1, 2, 2, 2, 2, 3 },
+                4,
+                -1
+            };
+
+            dataSet.Add(case_one);
+            dataSet.Add(case_two);
+            dataSet.Add(case_three);
+
+            return dataSet;
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(GetDataSet), DynamicDataSourceType.Method)]
+        public void SortedArrayWithIntegersAndNumber_Count_ReturnsNumberOfOccurancesInAnArray(int[] values, int number, int expected)
+        {
+            //Arrange
+            var solution = new CountNumberOfOccurrencesInSortedArray();
+
+            //Act
+            var actual = solution.Count(values, number);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
