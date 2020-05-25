f09acb14a1bf5ea8cc2300772064b7759c4d6ff0
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-25 22:16:38 +0530

    Made changes to README (#3)
    * Added binary search problems
    
    * Added ReadMe file
    
    * Add ReadMe file
    
    * Added new problem : Count number of occurances of a number in a sorted array.
    
    * Made changes to README
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

--- test/BinarySearchProblems.Tests/FindNumberInSortedArray/FindNumberInSortedArrayV1Tests.cs
+++ test/BinarySearchProblems.Tests/FindNumberInSortedArray/FindNumberInSortedArrayV1Tests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BinarySearchProblems.Tests
 {
     [TestClass]
-    public class FindNumberInSortedArrayTests
+    public class FindNumberInSortedArrayV1Tests
     {
         [TestMethod]
         public void GivenSortedArrayAndNumberToFind_Find_ReturnsIndexOfNumber()
@@ -11,7 +11,7 @@ namespace BinarySearchProblems.Tests
             //Arrange
             var values = new int[] { 1, 2, 3, 4, 5, 6, 7 };
             var number = 5;
-            var solution = new FindNumberInSortedArray();
+            var solution = new FindNumberInSortedArrayV1();
 
             //Act
             var index = solution.Find(values, number);
