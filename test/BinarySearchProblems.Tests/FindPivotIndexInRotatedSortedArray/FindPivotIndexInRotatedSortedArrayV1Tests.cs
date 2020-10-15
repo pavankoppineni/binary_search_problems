c13009c7b2224040f190be905d2c47939cacacc1
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-10-15 16:55:06 +0530

    Added new problem (#10)
    * Added binary search problems
    
    * Added ReadMe file
    
    * Add ReadMe file
    
    * Added new problem : Count number of occurances of a number in a sorted array.
    
    * Made changes to README
    
    * Added new solution
    
    * Added problem Find smallest missing element from a sorted array
    
    * Added two problems
    
    * Resolved unit tests
    
    * Added new problem
    
    * added new problems
    
    * Fixed unit testing
    
    * Fixed code issue
    
    * Added find pivot index
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ test/BinarySearchProblems.Tests/FindPivotIndexInRotatedSortedArray/FindPivotIndexInRotatedSortedArrayV1Tests.cs
@@ -0,0 +1,27 @@
+﻿using BinarySearchProblems.FindPivotIndexInRotatedSortedArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchProblems.Tests.FindPivotIndexInRotatedSortedArray
+{
+    [TestClass]
+    public class FindPivotIndexInRotatedSortedArrayV1Tests
+    {
+        [TestMethod]
+        public void Given_RotatedSortedArray_When_FindPivot_Then_ShouldReturnPivotIndexOfArray()
+        {
+            //Given
+            var values = new int[] { 7, 8, 9, 10, 1, 2 };
+            var problem = new FindPivotIndexInRotatedSortedArrayV1();
+            var expectedResult = 4;
+
+            //When
+            var actualResult = problem.FindPivot(values);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
